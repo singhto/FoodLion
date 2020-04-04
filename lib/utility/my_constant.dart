7022fe19c04197351c539f7a7c9f96c8b2aa4f10
lion <singhto1983@gmail.com>
2020-04-04 08:36:01 +0700

    Map long Eror

--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -1,7 +1,7 @@
 class MyConstant {
 
   String urlSaveFile = 'http://movehubs.com/app/saveFile.php';
-  String urtImagePath = 'http://movehubs.com/app/Shop/';
+  String urlImagePath = 'http://movehubs.com/app/Shop/';
   String urlAddUserShop = 'http://movehubs.com/app/addUserShop.php';
 
 
--- lib/widget/register_user.dart
+++ lib/widget/register_user.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:foodlion/utility/my_style.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class RegisterUser extends StatefulWidget {
   @override
@@ -6,10 +8,162 @@ class RegisterUser extends StatefulWidget {
 }
 
 class _RegisterUserState extends State<RegisterUser> {
+  // Field
+
+  // Method
+
+  Widget showMap() {
+    LatLng centerLatLng = LatLng(18.320698, 99.398269);
+    CameraPosition cameraPosition = CameraPosition(
+      target: centerLatLng,
+      zoom: 16.0,
+    );
+
+    return Container(
+      height: MediaQuery.of(context).size.height * 3,
+      child: GoogleMap(initialCameraPosition: cameraPosition,
+      onMapCreated: (value){},),
+    );
+  }
+
+  Widget nameForm() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 250.0,
+          child: TextField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.account_box),
+              hintText: 'Name :',
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget userForm() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 250.0,
+          child: TextField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.email),
+              hintText: 'User :',
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget passwordForm() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 250.0,
+          child: TextField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.lock_open),
+              hintText: 'Password :',
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget phoneForm() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 250.0,
+          child: TextField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.phone),
+              hintText: 'Phone :',
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget cameraButton() {
+    return OutlineButton.icon(
+      onPressed: () {},
+      icon: Icon(Icons.add_a_photo),
+      label: Text('Camera'),
+    );
+  }
+
+  Widget galleryButton() {
+    return OutlineButton.icon(
+      onPressed: () {},
+      icon: Icon(Icons.add_photo_alternate),
+      label: Text('Gallery'),
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        cameraButton(),
+        galleryButton(),
+      ],
+    );
+  }
+
+  Widget showPicture() {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: Image.asset('images/user.png'),
+    );
+  }
+
+  Widget showListView() {
+    return ListView(
+      padding: EdgeInsets.all(16.0),
+      children: <Widget>[
+        MyStyle().showTitle('User Picture'),
+        showPicture(),
+        showButton(),
+        MyStyle().mySizeBox(),
+        MyStyle().showTitle('Information'),
+        MyStyle().mySizeBox(),
+        nameForm(),
+        MyStyle().mySizeBox(),
+        userForm(),
+        MyStyle().mySizeBox(),
+        passwordForm(),
+        MyStyle().mySizeBox(),
+        phoneForm(),
+        MyStyle().mySizeBox(),
+        showMap(),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'This is User'
-    );
+    return showListView();
   }
 }
